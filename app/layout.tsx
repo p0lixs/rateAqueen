a25aca6cda1f4d3eac33e119667bb0175f27ce0d
Sergio <s.fu.urabayen@gmail.com>
2026-07-04 10:03:19 +0200

    feat: add @vercel/speed-insights dependency and integrate into layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import MobileNav from "@/components/mobile-nav";
-
+import { SpeedInsights } from "@vercel/speed-insights/next"
 export const metadata: Metadata = {
   title: "Rate a Queen",
   description: "Crea una clasificación anónima con tus amigas",
@@ -11,6 +11,7 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
   return (
     <html lang="es">
       <body>{children}<MobileNav /></body>
+      <SpeedInsights/>
     </html>
   );
 }
